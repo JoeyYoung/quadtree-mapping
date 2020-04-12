ebfe7e906f0cde93949eb9cc86524b6119c1d3cf
joeyyoung <1761889730@qq.com>
2020-04-12 16:28:31 +0800

    map test

--- app/ultra_simple/map_test.py
+++ app/ultra_simple/map_test.py
@@ -1,19 +1,60 @@
 from ctypes import *
+import os
+import sys
+import threading
+import time
 
-add_plus = CDLL('./libmap.so')
+step_size = 3.0
+forward_seconds = 5.0
+pi = 3.1415926
 
-x = c_float(2.0)
-y = c_float(2.0)
-theta = c_float(0.0)
+# define start position and theta
+# todo, current only apply to theta=0
+start_x = 20.0
+start_y = 5.0
+# walker_theta = 0.0
+walker_theta = 180.0
 
-add_plus.run_main_plus(x, y, theta)
+def loop_record(control):
+    def print_odo():
+        print("odometry x y theta: ", round(control.odo.X, 3), round(control.odo.Y, 3), round(control.odo.THETA, 3))
 
-# sum = add_plus.add_integer_plus(4,5)
-# print("sum: " + str(sum))
+    add_plus = CDLL('./libmap.so')
+    step = 0
 
-# # 使用float类型变量作为函数参数
-# a = c_float(5.5)
-# b = c_float(4.1)
-# add_float =  add_plus.add_float_plus
-# add_float.restype = c_float
-# sum_float = add_plus.add_float_plus(a,b)
+    while True:
+        temp = input()
+        # scan and store
+        print_odo()
+        if walker_theta == 0.0:
+            x = c_float(control.odo.X + start_x)
+            y = c_float(control.odo.Y + start_y)
+
+            if control.odo.THETA > 0:
+                theta = 360 - control.odo.THETA / pi * 180
+            else:
+                theta = control.odo.THETA / pi * 180
+
+        elif walker_theta == 180.0:
+            x = c_float(start_x - control.odo.X)
+            y = c_float(start_y - control.odo.Y)
+
+            if control.odo.THETA > 0:
+                theta = 180 - control.odo.THETA / pi * 180
+            else:
+                theta = 180 - control.odo.THETA / pi * 180
+
+        theta = c_float(theta)
+        add_plus.run_main_plus(x, y, theta, c_int(step))
+        step += 1
+
+
+if __name__ == '__main__':
+    cd = CD.ControlDriver(record_mode=True)
+    p1 = threading.Thread(target=loop_record, args=(cd,))
+    p2 = threading.Thread(target=cd.control_part, args=())
+
+    p2.start()
+    p1.start()
+
+    loop_record(cd)
