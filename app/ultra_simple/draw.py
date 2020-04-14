f151ba36afb90d17c738a2a3760d789994f1f4dd
joeyyoung <1761889730@qq.com>
2020-04-14 16:57:23 +0800

    test ok

--- app/ultra_simple/draw.py
+++ app/ultra_simple/draw.py
@@ -23,6 +23,16 @@ def read_points(inputdirs):
                     res.append(temp)
     return res
 
+def read_tree():
+    res = []
+    with open("/Users/xyzhao/Desktop/quadtree-mapping/lidar_tree", 'r') as f:
+        lines = f.readlines()
+        for i in range(len(lines)):
+            line = lines[i]
+            xy = line.split(";")
+            temp = [float(xy[0]), float(xy[1]), float(xy[2]), float(xy[3])]
+            res.append(temp)
+    return res
 
 if __name__ == '__main__':
     unit = 1000
@@ -32,16 +42,29 @@ if __name__ == '__main__':
         for j in range(unit):
             img[i][j] = 255
 
-    dirs = ["./test_step1"]
-    points = read_points(dirs)
+    # dirs = ["./test_step1"]
+    # points = read_points(dirs)
 
-    for point in points:
-        x = int(point[0] * 25)
-        y = int(point[1] * 25)
+    # for point in points:
+    #     x = int(point[0] * 25)
+    #     y = int(point[1] * 25)
 
-        if x > 1000 or y > 1000:
-            continue
-        img[x][y] = 0
+    #     if x > 1000 or y > 1000:
+    #         continue
+    #     img[x][y] = 0
+
+    grids = read_tree()
+    for grid in grids:
+        x1 = round(grid[0] * 25)
+        x2 = round(grid[1] * 25)
+        y1 = round(grid[2] * 25)
+        y2 = round(grid[3] * 25)
+
+        for x in range(x1, x2+1):
+            for y in range(y1, y2+1):
+                if x > 1000 or y > 1000:
+                    continue
+                img[x][y] = 0
 
     cv2.namedWindow("contour", cv2.WINDOW_NORMAL)
     cv2.imshow("contour", img)
